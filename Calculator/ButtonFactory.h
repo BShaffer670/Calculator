4a5dfd88319e31a33bbbd710ac076ddb56b814b3
Brandon Shaffer <shafferbrandon10@hotmail.com>
2022-06-15 13:32:21 -0400

    Updated Factory

--- Calculator/ButtonFactory.h
+++ Calculator/ButtonFactory.h
@@ -8,28 +8,10 @@ private:
 	cMain* calcFrame = nullptr;
 
 public:
-	ButtonFactory(cMain* _frame);
+	ButtonFactory();
 	~ButtonFactory();
 
-	wxButton* ZeroButton();
-	wxButton* OneButton();
-	wxButton* TwoButton();
-	wxButton* ThreeButton();
-	wxButton* FourButton();
-	wxButton* FiveButton();
-	wxButton* SixButton();
-	wxButton* SevenButton();
-	wxButton* EightButton();
-	wxButton* NineButton();
-	wxButton* AdditionButton();
-	wxButton* SubtractionButton();
-	wxButton* MultiplicationButton();
-	wxButton* DivisionButton();
-	wxButton* ModulusButton();
-	wxButton* EqualsButton();
-	wxButton* BinaryButton();
-	wxButton* HexButton();
-	wxButton* DecimalButton();
-	wxButton* ClearButton();
+	wxButton* MakeButton(cMain* _frame, int _id, wxString _label, int x, int y);
+	wxButton* ClearButton(cMain* _frame);
 };
 
